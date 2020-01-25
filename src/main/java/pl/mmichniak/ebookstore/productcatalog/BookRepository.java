1606903ac2816e36386f35bb5237b6133d6e4851
s-301-08 <s-301-08@gumiforest.uek.krakow.pl>
2020-01-25 14:06:27 +0100

    Add @Repository annotation in BookRepository

--- src/main/java/pl/mmichniak/ebookstore/productcatalog/BookRepository.java
+++ src/main/java/pl/mmichniak/ebookstore/productcatalog/BookRepository.java
@@ -1,7 +1,8 @@
 package pl.mmichniak.ebookstore.productcatalog;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
 }
