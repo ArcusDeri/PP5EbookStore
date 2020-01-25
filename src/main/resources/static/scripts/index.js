1b1d29ba7e05c2cde9390c2929a58bc488111b3e
s-301-08 <s-301-08@gumiforest.uek.krakow.pl>
2020-01-25 15:43:15 +0100

    improve product listing

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -5,12 +5,19 @@ const init = () => {
             .then(response => response.json())
             .then(products => products.forEach(product => {
                 const listElement = document.createElement("li");
-                listElement.innerHTML = `${product.title} (${product.description})`;
-
+                const h1Element = document.createElement("h1");
+                const paragraphElement = document.createElement("p");
                 const imgElement = document.createElement("img");
+
+                h1Element.innerText = product.title;
+                paragraphElement.innerText = product.description;
+
                 imgElement.setAttribute("alt","book cover");
                 imgElement.setAttribute("src",product.coverUrl);
 
+                listElement.appendChild(h1Element);
+                listElement.appendChild(paragraphElement);
+                listElement.appendChild(document.createElement("br"));
                 listElement.appendChild(imgElement);
                 listNode.appendChild(listElement);
             }));
